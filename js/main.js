0845e009b671b741cbe340d2f6d3d6755439e77a
akhtaruzzaman <akhtar01947@gmail.com>
2021-01-30 18:40:34 +0600

    some-update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-$(".navbar-menu").click(function() {
-    $(".navbar-menu-item").slideToggle();
-});
 $(document).ready(function() {
+    $(".navbar-menu").click(function() {
+        $(".navbar-menu-item").slideToggle();
+    });
     $(".hero-main-wrap").owlCarousel({
         items: 1,
         loop: true,
         autoplay: true,
     });
+
 });
+$('#typed_1').t({
+    speed: 35,
+    repeat: true,
+});
+$('#typed_2').t({
+    speed: 35,
+    repeat: true,
+});
+$('#typed_3').t({
+    speed: 35,
+    repeat: true,
+});
+$('#s_bar-bg').t();
