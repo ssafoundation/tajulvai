9f33acce8e91e67157514dfded31697ebca6d861
akhtaruzzaman <akhtar01947@gmail.com>
2021-02-01 03:39:02 +0600

    file-update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
-    $(".navbar-menu").click(function() {
+    $(".navbar-menu-mobile-btn").click(function() {
         $(".navbar-menu-item").slideToggle();
     });
-    $(".hero-main-wrap").owlCarousel({
-        items: 1,
-        loop: true,
+
+    $(".slider-image").slick({
+        slidesToShow: 1,
         autoplay: true,
+        autoplaySpeed: 3000,
+    });
+    $(".text-typed-wrap").slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+    $('#typed_1').t({
+        speed: 35,
+        repeat: true,
+    });
+    $('#typed_2').t({
+        speed: 35,
+        repeat: true,
+    });
+    $('#typed_3').t({
+        speed: 35,
+        repeat: true,
     });
 
+
 });
-$('#typed_1').t({
-    speed: 35,
-    repeat: true,
-});
-$('#typed_2').t({
-    speed: 35,
-    repeat: true,
-});
-$('#typed_3').t({
-    speed: 35,
-    repeat: true,
-});
-$('#s_bar-bg').t();
